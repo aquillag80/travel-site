5e9b5a87471aea02c36440c0508eae78f78b41b1
aquillag80 <159091676+aquillag80@users.noreply.github.com>
2024-06-17 14:30:34 -0400

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,34 @@
+#Variables
+x = 5
+y = "Guy Fieri"
+
+#camelCase
+firstName = "Guy"
+
+#print datea type with type
+x = float(x)
+
+isHot = True
+
+print(type(firstName))
+print(type(x))
+print(type(isHot))
+
+float('6')
+
+#Modulus
+print(5 % 3)
+
+#Exponentiation
+print(5 ** 3)
+
+result = ((4+3)*4**2/(6-4)+5)
+print(result)
+
+result = ((4+3)*4**2/(6-4)+5) % 60
+print(result)
+
+#find the remainder
+twelve = 12
+rem = twelve % 2
+print(rem)
