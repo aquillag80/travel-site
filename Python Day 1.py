9f0c784c434f581921cfdd76541771df5068876d
aquillag80 <159091676+aquillag80@users.noreply.github.com>
2024-06-17 13:37:50 -0400

    Add files via upload

---
+++ Python Day 1.py
@@ -0,0 +1,34 @@
+print ("Hello, World!")
+
+greeting = "Hello"
+name = "Guy Fieri"
+print(greeting + name)
+
+greeting = "Hi"
+firstname = "Guy"
+lastname = "Fieri"
+print(greeting, firstname, lastname)
+#concatenation with a space
+age = 56
+''' 3 single quotes creates a multiline comment
+#use , to automatically insert spaces
+print(name, "is", age, "years old.")
+
+print(name + "is" + str(age) + "years old.")
+
+#concatenation with a space
+print(greeting + " " + name)
+
+one = "1"
+two = "2"
+print(one + two)
+
+print(1 + 2)
+
+#type casting from number
+print("1" + str(2))
+'''
+
+#f-string: format string
+place = "Charleston"
+print(f"{name} welcomes you to {place}!")
